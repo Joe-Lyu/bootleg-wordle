c2736b69f23ea894b2f08207797509acb2780675
Tom Wang <tomwxh@163.com>
2023-09-23 14:15:46 -0400

    add doc for function

--- utils.py
+++ utils.py
@@ -1,16 +1,27 @@
 import pandas as pd
 import random
-#🟩🟨⬜
-def cp_words(word,answer):
+
+def cp_words(word, answer):
+    """
+    Compare the guessed word to the answer and provide hints based on matching characters.
+
+    Args:
+        word (str): The guessed word.
+        answer (str): The correct word.
+
+    Returns:
+        str: A hint string containing symbols 🟩 (for correct letters in the correct position),
+             🟨 (for correct letters in the wrong position), or ⬜ (for incorrect letters).
+    """
     assert len(word) == len(answer) == 5, """mismatched word length, attempted word {} with length {}, 
-                                            answer {} with length {}, should be both 5""".format(word,len(word),answer,len(answer))
-    hint = ["⬜"]*5
+                                            answer {} with length {}, should be both 5""".format(word, len(word), answer, len(answer))
+    hint = ["⬜"] * 5
     word = list(word)
     answer = list(answer)
     for i in range(5):
         if word[i] == answer[i]:
             hint[i] = '🟩'
-            word[i],answer[i] = '_','_'
+            word[i], answer[i] = '_', '_'
     for i in range(5):
         char = word[i]
         if char != '_':
@@ -21,36 +32,75 @@ def cp_words(word,answer):
     return ''.join(hint)
 
 
+DF = pd.read_csv('scores.csv', index_col=0)
 
-DF = pd.read_csv('scores.csv',index_col=0)
 class score_dict:
-    def __init__(self,DF=DF):
+    """
+    Class to handle word data from a DataFrame, including word scores, frequencies, and difficulties.
 
+    Attributes:
+        DF (DataFrame): DataFrame sorted by word frequency.
+        sorted_words (list): List of words classified as 'La', sorted by frequency.
+        vocab (int): Number of words in sorted_words.
+        easy_words (list): Bottom third of sorted_words.
+        medium_words (list): Middle third of sorted_words.
+        hard_words (list): Top third of sorted_words.
+        all_words (list): List of all words sorted by score in descending order.
+        alt_DF (DataFrame): DataFrame sorted by 'alt_score'.
+        alt_all_words (list): List of all words sorted by 'alt_score' in ascending order.
+    """
+    def __init__(self, DF=DF):
         self.DF = DF.sort_values('freq').reset_index()
-        self.sorted_words = list(self.DF.loc[self.DF['class']=='La']['word'])
+        self.sorted_words = list(self.DF.loc[self.DF['class'] == 'La']['word'])
         self.vocab = len(self.sorted_words)
-        self.easy_words = self.sorted_words[self.vocab//3*2:]
-        self.medium_words = self.sorted_words[self.vocab//3:self.vocab//3*2]
-        self.hard_words = self.sorted_words[:self.vocab//3]
-        self.DF = DF.sort_values(['score'],ascending=[False]).reset_index()
+        self.easy_words = self.sorted_words[self.vocab // 3 * 2:]
+        self.medium_words = self.sorted_words[self.vocab // 3:self.vocab // 3 * 2]
+        self.hard_words = self.sorted_words[:self.vocab // 3]
+        self.DF = DF.sort_values(['score'], ascending=[False]).reset_index()
         self.all_words = list(self.DF['word'])
-        self.alt_DF = DF.sort_values(['alt_score'],ascending=[True])
+        self.alt_DF = DF.sort_values(['alt_score'], ascending=[True])
         self.alt_all_words = list(self.alt_DF['word'])
 
-    
-    def get_score(self,word):
+    def get_score(self, word):
+        """
+        Retrieve the score of a word.
+
+        Args:
+            word (str): The word whose score is to be fetched.
+
+        Returns:
+            float: Score of the word.
+        """
         assert word in self.all_words, "Word is not in Wordle dictionary"
-        row = self.DF.loc[self.DF['word']==word]
+        row = self.DF.loc[self.DF['word'] == word]
         return list(row['score'])[-1]
-    
-    def get_freq(self,word):
+
+    def get_freq(self, word):
+        """
+        Retrieve the frequency of a word.
+
+        Args:
+            word (str): The word whose frequency is to be fetched.
+
+        Returns:
+            int: Frequency of the word.
+        """
         assert word in self.all_words, "Word is not in Wordle dictionary"
-        row = self.DF.loc[self.DF['word']==word]
+        row = self.DF.loc[self.DF['word'] == word]
         return list(row['freq'])[-1]
-    
-    def get_difficulty(self,difficulty='random'):
+
+    def get_difficulty(self, difficulty='random'):
+        """
+        Retrieve a word based on the specified difficulty.
+
+        Args:
+            difficulty (str, optional): Difficulty level (choices: 'random', 'easy', 'medium', 'hard'). Defaults to 'random'.
+
+        Returns:
+            str: A word of the specified difficulty.
+        """
         self.DF.sort_values('freq')
-        if difficulty =='random':
+        if difficulty == 'random':
             return random.choice(self.sorted_words)
         elif difficulty == 'easy':
             return random.choice(self.easy_words)
@@ -60,6 +110,3 @@ class score_dict:
             return random.choice(self.hard_words)
         else:
             raise ValueError("invalid difficulty param")
-    
-
-        
--- wordle_core.py
+++ wordle_core.py
@@ -1,39 +1,72 @@
-from utils import cp_words,score_dict
-from itertools import permutations,combinations_with_replacement
+from utils import cp_words, score_dict
+from itertools import permutations, combinations_with_replacement
 from statistics import stdev
+
 sd = score_dict()
 words = sd.sorted_words
 MAX_TRIES = 5
 
+
 def word_input(guesses):
+    """
+    Prompt the user for a word guess.
+
+    Args:
+        guesses (int): The current guess attempt number.
+
+    Returns:
+        str: The word provided by the user that exists in the predefined word list.
+    """
     word = input("Guess {}:\t".format(guesses))
     while word not in words:
         print("That is not a recognized word. Please retry.")
         word = input("Guess {}:\t".format(guesses))
     return word
 
-def main(answer,MAX_TRIES = MAX_TRIES):
+
+def main(answer, MAX_TRIES=MAX_TRIES):
+    """
+    Run the Wordle game, comparing the user's guesses against the provided answer.
+
+    Args:
+        answer (str): The correct word that the user needs to guess.
+        MAX_TRIES (int, optional): Maximum number of tries for the user to guess the word. Defaults to 5.
+    """
     guesses = 1
     word = word_input(guesses)
 
     while word != answer and guesses <= MAX_TRIES:
-        hints = cp_words(word,answer)
+        hints = cp_words(word, answer)
         print(hints)
         guesses += 1
         word = word_input(guesses)
 
     if word == answer:
-        print('🟩'*5)
+        print('🟩' * 5)
         print("You guessed it in {} tries".format(guesses))
-
     else:
         print("You failed! The answer is {}".format(answer))
 
-def rank_words(filtered_sorted_words,hint,guess,alg='alt_score',symbolset=True,recursive=True):
+
+def rank_words(filtered_sorted_words, hint, guess, alg='alt_score', symbolset=True, recursive=True):
+    """
+    filter words based on given hints(outcome) and the chosen algorithm.
+
+    Args:
+        filtered_sorted_words (list): List of words filtered based on previous guesses and hints.
+        hint (list): List of symbols indicating the status of previous guesses.
+        guess (str): The previous word guess associate with the hint.
+        alg (str, optional): The chosen ranking algorithm. Defaults to 'alt_score'.
+        symbolset (bool, optional): Flag to use emoji symbols or plain text symbols. Defaults to True.
+        recursive (bool, optional): Flag to perform recursive ranking. Defaults to True.
+
+    Returns:
+        list: A filtered list of words taht still satisfy the specified criteria (hint, guess).
+    """
     if symbolset:
-        symbols = ['🟩','🟨','⬜']
+        symbols = ['🟩', '🟨', '⬜']
     else:
-        symbols = ['G','Y','?']
+        symbols = ['G', 'Y', '?']
     new_filter = []
     for sw in filtered_sorted_words:
         conds = True
@@ -42,22 +75,33 @@ def rank_words(filtered_sorted_words,hint,guess,alg='alt_score',symbolset=True,r
                 conds = conds and sw[i] == guess[i]
             elif hint[i] == symbols[1]:
                 conds = conds and guess[i] in sw and sw[i] != guess[i]
-            elif guess[i] not in guess[:i] and guess[i] not in guess[i+1:]:
+            elif guess[i] not in guess[:i] and guess[i] not in guess[i + 1:]:
                 conds = conds and guess[i] not in sw
             else:
                 conds = conds and sw[i] != guess[i]
         if conds:
             new_filter.append(sw)
-    if recursive and alg=='alt_score':
-        new_filter.sort(key=lambda w: get_remaining(w,new_filter))
+    if recursive and alg == 'alt_score':
+        new_filter.sort(key=lambda w: get_remaining(w, new_filter))
     return new_filter
 
-def get_remaining(guess,filtered_sorted_words=words):
+
+def get_remaining(guess, filtered_sorted_words=words):
+    """
+    Score the guess based on calculation of the remaining possible words.
+
+    Args:
+        guess (str): The current word guess.
+        filtered_sorted_words (list, optional): List of words filtered based on previous guesses and hints. Defaults to the full word list.
+
+    Returns:
+        float: Score indicating the number of remaining possibilities based on the guess.
+    """
     all_possible_hints = []
-    for p in list(combinations_with_replacement(['G','Y','?'],5)):
+    for p in list(combinations_with_replacement(['G', 'Y', '?'], 5)):
         all_possible_hints += list(set(permutations(p)))
     possibilities_left = []
     for hint in all_possible_hints:
-        possibilities_left.append(len(rank_words(filtered_sorted_words,hint,guess,symbolset=False,recursive=False)))
+        possibilities_left.append(len(rank_words(filtered_sorted_words, hint, guess, symbolset=False, recursive=False)))
     score = stdev(possibilities_left) * sum(possibilities_left) / 1000
     return score
